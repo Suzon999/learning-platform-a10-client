c135296466bec21a22e32ffc696836383e451dd5
Suzon999 <ashikurrahamansuzon346112.com>
2022-10-26 21:35:06 +0600

    banner added home

--- src/Pages/components/Home.js
+++ src/Pages/components/Home.js
@@ -8,11 +8,11 @@ const Home = () => {
             <section className="dark:bg-gray-800 dark:text-gray-100 lg:px-20 px-5">
                 <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
                     <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
-                        <h1 className="text-4xl font-bold leading-none sm:text-4xl lg:text-6xl"> <span className="dark:text-amber-600">Web</span>-Learning
+                        <h1 className="text-4xl font-bold leading-none sm:text-4xl lg:text-6xl"> <span className="dark:text-amber-600">Web</span>-Tecnology
                             <span className="text-2xl text-amber-600">.bd</span><br /> <p className='py-3 '>Platfrom 2<span className="text-amber-600">0</span>2<span className="text-amber-600">2</span></p>
                         </h1>
                         <p className="mt-6 mb-8 text-lg lg:text-3xl sm:mb-12">Mr.Sundor Pichai <span className='text-amber-500'>CEO</span>
-                            
+
                         </p>
                         <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
                             <Link to='/courses' rel="noopener noreferrer" href="#" className="px-8 py-3 text-lg font-semibold rounded dark:bg-violet-400 dark:text-gray-900">Our Courses</Link>
